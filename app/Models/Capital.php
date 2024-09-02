9f443b93637e4694153cf665d740210d915493cd
Darwing27 <dsrondon96@misena.edu.co>
2024-09-02 12:32:41 -0500

    first commit

---
+++ app/Models/Capital.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Capital extends Model
+{
+    //
+    protected $table = 'capital';
+
+    protected $fillable = ['detalle','idDepartamento'];
+
+    public $timestamps = false;
+}
